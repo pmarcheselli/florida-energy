a4d74c287cf67a1788f7f1c6621431e401354595
Pmarch <pmarcheselli@gmail.com>
2018-05-30 23:20:37 -0700

    fixed logo link issue

--- scripts/vue-script.js
+++ scripts/vue-script.js
@@ -2,40 +2,40 @@ console.log("hello")
 
 var logos = [
   {name: "University of Florida",
-   imgSource: "logo_01.jpg",
+   imgSource: "images/logo_01.jpg",
    url: "http://www.ufl.edu/"
   }, {name: "Florida State University",
-   imgSource: "logo_02.jpg",
+   imgSource: "images/logo_02.jpg",
    url: "http://www.fsu.edu/"
   }, {name: "University of Central Florida",
-   imgSource: "logo_03.jpg",
+   imgSource: "images/logo_03.jpg",
    url: "http://www.ucf.edu/"
   }, {name: "University of Southern Florida",
-   imgSource: "logo_04.jpg",
+   imgSource: "images/logo_04.jpg",
    url: "http://www.usf.edu/"
   }, {name: "Florida A&M University",
-   imgSource: "logo_05.jpg",
+   imgSource: "images/logo_05.jpg",
    url: "http://www.famu.edu/"
   }, {name: "Florida Atlantic Univeristy",
-   imgSource: "logo_06.jpg",
+   imgSource: "images/logo_06.jpg",
    url: "http://www.fau.edu/"
   }, {name: "Florida Gulf Coast University",
-   imgSource: "logo_07.jpg",
+   imgSource: "images/logo_07.jpg",
    url: "http://www.fgcu.edu/"
   }, {name: "Florida International University",
-   imgSource: "logo_08.jpg",
+   imgSource: "images/logo_08.jpg",
    url: "http://www.fiu.edu/"
   }, {name: "New College of Florida", 
-   imgSource: "logo_09.jpg",
+   imgSource: "images/logo_09.jpg",
    url: "http://www.ncf.edu/"
   }, {name: "University of North Florida",
-   imgSource: "logo_10.jpg",
+   imgSource: "images/logo_10.jpg",
    url: "http://www.unf.edu/"
   }, {name: "University of West Florida",
-   imgSource: "logo_11.jpg",
+   imgSource: "images/logo_11.jpg",
    url: "http://www.uwf.edu/"
   }, {name: "Florida Polytechnic",
-   imgSource: "logo_12.jpg",
+   imgSource: "images/logo_12.jpg",
    url: "https://floridapolytechnic.org/"
   }
 ]
